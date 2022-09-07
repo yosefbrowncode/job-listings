8ef999c27d27849dfb6bd110b500c4800423fd7c
Yosef Brown <yosef@aklaunch.com>
2022-09-07 03:02:27 +0300

    closer and closer

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -84,42 +84,42 @@ export default {
 
  <template>
  <div class="cardDicv mt-20 mb-40 pb-40" >
-    <div v-for="job in filteredSearch" :key="job.id" class="container mx-auto h-40 w-9/12 rounded-md bg-300 flex flex-row shadow-xl bg-my-white mt-5">
-            <div class="bg-my-cyan w-1.5 rounded-l-md" v-if="job.featured"></div>
-            <div class="basis-1/2 my-auto flex flex-row">
-            <img class="w-24 h-124 align-middle  ml-9 rounded-full" :src="job.logo" alt="">
-     
-            <div class="container flex flex-col">
-                <div class="basis-1/2 container h-fit flex flex-row space-x-4 ml-8">
-                    <p class="font-bold text-base text-my-cyan h-fit">{{job.company}}</p>
-                    <p class="rounded-full py-1 px-1.5 bg-my-cyan h-fit text-my-white cardFont text-xs font-bold" v-if="job.new">NEW!</p>
-                    <p class="rounded-full py-1 px-1.5 bg-dark-tag h-fit text-my-white cardFont text-xs font-bold" v-if="job.featured">FEATURED</p>
-                </div>
+    <div v-for="job in filteredSearch" :key="job.id" class="container mx-auto h-60 xl:h-40 w-9/12 rounded-md bg-300 flex flex-col xl:flex-row shadow-xl bg-my-white mt-5">
+            <div class="bg-my-cyan w-1.5 rounded-l-md flex   " v-if="job.featured"></div>
+                <div class="xl:basis-1/2 my-auto flex ">
+                    <img class="w-24 h-124 align-middle  ml-9 rounded-full" :src="job.logo" alt="">
             
-                <div><h2 class="font-extrabold text-lg text-dark-tag text-left ml-8 cardFont">{{job.position}}</h2></div>
-                <div class="basis-1/2 container h-fit flex flex-row space-x-4 ml-8 mt-1">
-                    <p class=" text-sm font-medium text-my-cyan h-fit text-light-tag">{{job.postedAt}}</p>
-                    <span class="text-light-tag">&#9679;</span>
-                    <p class="h-fit   text-sm font-medium text-light-tag">{{job.contract}}</p>
-                    <span class="text-light-tag">&#9679;</span>
-                    <p class="h-fit   text-sm font-medium text-light-tag ">{{job.location}}</p>
-                </div>
+                    <div class="container flex flex-col">
+                        <div class="basis-full basis-1/2 container h-fit flex flex-row space-x-4 ml-8">
+                            <p class="font-bold text-base text-my-cyan h-fit">{{job.company}}</p>
+                            <p class="rounded-full py-1 px-1.5 bg-my-cyan h-fit text-my-white cardFont text-xs font-bold" v-if="job.new">NEW!</p>
+                            <p class="rounded-full py-1 px-1.5 bg-dark-tag h-fit text-my-white cardFont text-xs font-bold" v-if="job.featured">FEATURED</p>
+                        </div>
+                    
+                        <div><h2 class="font-extrabold text-lg text-dark-tag text-left ml-8 cardFont">{{job.position}}</h2></div>
+                        <div class="basis-1/2 container h-fit flex flex-row space-x-4 ml-8 mt-1">
+                            <p class=" text-sm font-medium text-my-cyan h-fit text-light-tag">{{job.postedAt}}</p>
+                            <span class="text-light-tag">&#9679;</span>
+                            <p class="h-fit   text-sm font-medium text-light-tag">{{job.contract}}</p>
+                            <span class="text-light-tag">&#9679;</span>
+                            <p class="h-fit   text-sm font-medium text-light-tag ">{{job.location}}</p>
+                        </div>
 
+
+                    </div>
+            
 
             </div>
-            
 
-            </div>
-
-            <div class="basis-1/2 flex space-x-4 my-auto justify-end mr-10">
+            <div class="basis-full xl:basis-1/2 flex space-x-4 my-auto  justify-end mr-10">
          
-                <p class=" bg-tag-color p-2 rounded-md text-my-cyan  font-semibold text-sm cardFont hover:bg-my-cyan   hover:text-my-white cursor-pointer" @click="roleFunc(job.role)">{{job.role}}</p>
-                <p class="level bg-tag-color p-2 rounded-md text-my-cyan  font-semibold text-sm cardFont hover:bg-my-cyan   hover:text-my-white cursor-pointer" @click="levelFunc(job.level)">{{job.level}}</p>
+                <p class=" bg-tag-color p-2 rounded-md text-my-cyan h-fit font-semibold text-sm cardFont hover:bg-my-cyan   hover:text-my-white cursor-pointer" @click="roleFunc(job.role)">{{job.role}}</p>
+                <p class="level bg-tag-color p-2 rounded-md text-my-cyan h-fit  font-semibold text-sm cardFont hover:bg-my-cyan   hover:text-my-white cursor-pointer" @click="levelFunc(job.level)">{{job.level}}</p>
                 <template v-for="lang in job.languages">
-                    <p class="languages bg-tag-color p-2 rounded-md text-my-cyan  font-semibold text-sm cardFont hover:bg-my-cyan   hover:text-my-white cursor-pointer" @click="langFunc(lang)">{{lang}}</p>
+                    <p class="languages bg-tag-color p-2 rounded-md text-my-cyan h-fit font-semibold text-sm cardFont hover:bg-my-cyan   hover:text-my-white cursor-pointer" @click="langFunc(lang)">{{lang}}</p>
                 </template>
                 <template v-for="tool in job.tools">
-                    <p class="languages bg-tag-color p-2 rounded-md text-my-cyan  font-semibold text-sm cardFont hover:bg-my-cyan   hover:text-my-white cursor-pointer" @click="toolFunc(tool)">{{tool}}</p>
+                    <p class="languages bg-tag-color p-2 rounded-md text-my-cyan h-fit  font-semibold text-sm cardFont hover:bg-my-cyan   hover:text-my-white cursor-pointer" @click="toolFunc(tool)">{{tool}}</p>
                 </template>
             </div>
     </div>
